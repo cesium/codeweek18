f7a067756ba48c7da40d55bf21f3d752aacac107
Nelson Estevão <nelsonmestevao@gmail.com>
2018-10-06 16:48:00 +0100

    Update CoderDojo Braga section

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,3 +80,21 @@
   });
 
 }(jQuery));
+
+/* CoderDojo Carousel
+  ========================================================*/
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("coderdojo-braga");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
